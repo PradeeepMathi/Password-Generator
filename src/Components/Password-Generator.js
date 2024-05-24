da848cefbf90ef90969f6a378f7e0d711ed07e31
Pradeepmathi <pradeepmathi1656@gmail.com>
2024-05-24 18:06:49 +0530

    html for password generator

--- src/Components/Password-Generator.js
+++ src/Components/Password-Generator.js
@@ -1,6 +1,34 @@
-export default function PasseordGenerator(){
-    return(
-        <>
-        <h1>jj</h1></>
-    );
+export default function PasseordGenerator() {
+  return (
+    <>
+      <div className="password-generator">
+        <h2>Strong Password Generator</h2>
+        <div className="input-group">
+          <label htmFor="num">Password Generator</label>
+          <input type="number" id="num" />
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" id="upper" />
+          <label htmlFor="upper">Include UpperCase</label>
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" id="lower" />
+          <label htmlFor="lower">Include LowerCase</label>
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" id="numbers" />
+          <label htmlFor="numbers">Include Numbers</label>
+        </div>
+        <div className="checkbox-group">
+          <input type="checkbox" id="symbol" />
+          <label htmlFor="symbol">Include Symbol</label>
+        </div>
+        <button className="generate-btn">Generate Password</button>
+        <div className='generated-password'>
+            <input type='text' readOnly />
+            <button className='copy-btn'>Copy</button>
+        </div>
+      </div>
+    </>
+  );
 }
